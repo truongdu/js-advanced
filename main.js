304dd5b10749b9789ec17884c87f2ea480af30d3
TruongDu <truongdu.td88@gmailcom>
2020-05-01 14:44:32 +0700

    viet string + ten bien' ma khong dung noi chuoi

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var button = document.querySelector(".btn-alert");
 var tom = {
 	name: "TOM",
 	sayHello: function() {
-		alert("My name is " + this.name);
+		// alert("My name is " + this.name);
+		alert(`My name is ${this.name}`); // chac viet thu 2 ko dung noi chuoi, co the xuong hang duoc.
 	}
 };
 button.addEventListener("click", tom.sayHello.bind(tom));
